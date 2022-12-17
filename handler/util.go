edd34c16c65e648df662ecd20993a1f701b3a63e
John A. Stevens <john@otherskylines.com>
2022-12-16 20:42:25 -0700

    Export functions

--- handler/util.go
+++ handler/util.go
@@ -8,17 +8,17 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func handlerLogger(r *http.Request, handler string) *logrus.Entry {
+func HandlerLogger(r *http.Request, handler string) *logrus.Entry {
 	log := r.Context().Value("logger").(*logrus.Entry).WithField("handler", handler)
 	log.Info("Handler called")
 	return log
 }
 
-func logHandlerReturn(log *logrus.Entry) {
+func LogHandlerReturn(log *logrus.Entry) {
 	log.Info("Handler returned")
 }
 
-func unmarshalRequestBody(log *logrus.Entry, r *http.Request, value interface{}) error {
+func UnmarshalRequestBody(log *logrus.Entry, r *http.Request, value interface{}) error {
 	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
@@ -34,21 +34,21 @@ func unmarshalRequestBody(log *logrus.Entry, r *http.Request, value interface{})
 	return nil
 }
 
-func respondError(log *logrus.Entry, w http.ResponseWriter, err error, httpResponseCode int) {
+func RespondError(log *logrus.Entry, w http.ResponseWriter, err error, httpResponseCode int) {
 	log.WithError(err).WithField("httpResponseCode", httpResponseCode).Error()
 	http.Error(w, err.Error(), httpResponseCode)
 }
 
-func respondInternalServerError(log *logrus.Entry, w http.ResponseWriter, err error) {
-	respondError(log, w, err, 500)
+func RespondInternalServerError(log *logrus.Entry, w http.ResponseWriter, err error) {
+	RespondError(log, w, err, 500)
 }
 
-func respondJSON(log *logrus.Entry, w http.ResponseWriter, value interface{}) {
+func RespondJSON(log *logrus.Entry, w http.ResponseWriter, value interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 
 	jsonResp, err := json.Marshal(value)
 	if err != nil {
-		respondError(log, w, err, 500)
+		RespondError(log, w, err, 500)
 		return
 	}
 
