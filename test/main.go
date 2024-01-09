70805cc8362d09c52954f5b3602d198ab2ae4cac
John A. Stevens <john@otherskylines.com>
2024-01-09 16:46:00 -0700

    Add additional tests for context and logging

--- test/main.go
+++ test/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/sjohna/go-server-common/log"
+import (
+	"context"
+	"github.com/sjohna/go-server-common/handler"
+	"github.com/sjohna/go-server-common/log"
+	"github.com/sjohna/go-server-common/repo"
+	"github.com/sjohna/go-server-common/service"
+	"net/http"
+)
 
 func main() {
 	logger, configLogger := log.GetApplicationLoggers("testOutput/serverCommonTest", "go-server-common-test")
@@ -12,4 +19,35 @@ func main() {
 	logger.WithField("test", struct{ Test string }{"test"}).Panic("Test struct field")
 	logger.WithField("test", map[string]string{"test": "test"}).Panic("Test map field")
 	logger.WithField("test", []string{"test", "test"}).Panic("Test slice field")
+
+	testContext := context.WithValue(context.Background(), "logger", logger)
+
+	testRequest, _ := http.NewRequestWithContext(testContext, "GET", "http://localhost:8080", nil)
+
+	TestHandlerFunction(testRequest)
+}
+
+func TestHandlerFunction(req *http.Request) {
+	handlerContext, logger := handler.HandlerContext(req, "TestHandlerFunction")
+	defer handler.LogHandlerReturn(logger)
+
+	logger.Info("In TestHandlerFunction")
+
+	TestServiceFunction(handlerContext)
+}
+
+func TestServiceFunction(ctx context.Context) {
+	serviceContext, logger := service.ServiceFunctionContext(ctx, "TestServiceFunction")
+	defer service.LogServiceReturn(logger)
+
+	logger.Info("In TestServiceFunction")
+
+	TestRepoFunction(serviceContext)
+}
+
+func TestRepoFunction(ctx context.Context) {
+	_, logger := repo.RepoFunctionContext(ctx, "TestRepoFunction")
+	defer repo.LogRepoReturn(logger)
+
+	logger.Info("In TestRepoFunction")
 }
